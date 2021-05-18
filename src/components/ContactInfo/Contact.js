ab3eecbf042469750f4b892607ddd7b04bd63db4
Marble <mrivera0014@gmail.com>
2021-05-17 21:10:27 -0600

    added simple styling to contact form, will need to go through and make it more modern

--- src/components/ContactInfo/Contact.js
+++ src/components/ContactInfo/Contact.js
@@ -5,17 +5,16 @@ function Contact() {
     return (
         <div className='body'>
             <h1 className='contactTitle'>Contact Me</h1>
-            <form>
-                <h3 className="form-text header">Contact Me</h3>
-                <div className="mb-3">
+            <form className='contactForm'>
+                <div className="nameSection">
                     <p className="formText">Name:</p>
                     <input type="text" className="userInput" id="exampleinputname" />
                 </div>
-                <div className="mb-3">
+                <div className="emailSection">
                     <p className="formText">Email:</p>
                     <input type="email" className="userInput" id="exampleInputEmail1" />
                 </div>
-                <div className="mb-3">
+                <div className="messageSection">
                     <p className="formText">Message:</p>
                     <textarea className="userInput" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
