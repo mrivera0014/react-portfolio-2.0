61a8e435c1e52b4c32201e2ac04397b7f62d1813
Marble <mrivera0014@gmail.com>
2022-01-25 21:03:34 -0700

    created sass files to begin styling contact me section

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -1,18 +1,27 @@
 import React from "react";
+import * as FaIcons from 'react-icons/fa'
+
+import './ContactInfo.css'
 
 function ContactInfo() {
     return (
         <div className="body">
             <h1 className="projectsTitle"> Contact Me</h1>
-            <section>
-                <form action="https://www.linkedin.com/in/marblerivera/" method="get" target="_blank">
-                    <button type="submit">LinkedIn</button>
+            <section className="contactBtnHome">
+                <form className="contactBtns" action="https://www.linkedin.com/in/marblerivera/" method="get" target="_blank">
+                    <button type="submit"><FaIcons.FaLinkedin className='liIcon' />LinkedIn</button>
+                </form>
+                <form className="contactBtns" action="https://github.com/mrivera0014" method="get" target="_blank">
+                    <button type="submit"> <FaIcons.FaGithub className='ghIcon' />GitHub</button>
+                </form>
+                <form className="contactBtns" action="https://drive.google.com/file/d/14prWGh1MwMsED8Jh8WG93YCji29t9167/view?usp=sharing" method="get" target="_blank">
+                    <button type="submit"> <FaIcons.FaRegFilePdf className='ghIcon' />Resume</button>
                 </form>
 
             </section>
 
 
-        </div>
+        </div >
 
 
 
