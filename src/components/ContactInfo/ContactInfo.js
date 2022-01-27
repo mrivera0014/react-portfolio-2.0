0efe928bd7211cec435cd620db115352e6b8b07f
Marble <mrivera0014@gmail.com>
2022-01-26 20:28:30 -0700

    all contact button functional and styled

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -9,13 +9,13 @@ function ContactInfo() {
             <h1 className="projectsTitle"> Contact Me</h1>
             <section className="contactBtnHome">
                 <form className="contactBtns" action="https://www.linkedin.com/in/marblerivera/" method="get" target="_blank">
-                    <button type="submit"><FaIcons.FaLinkedin className='liIcon' />LinkedIn</button>
+                    <button className="linkedBtn" type="submit"><FaIcons.FaLinkedin className='liIcon' />LinkedIn</button>
                 </form>
                 <form className="contactBtns" action="https://github.com/mrivera0014" method="get" target="_blank">
-                    <button type="submit"> <FaIcons.FaGithub className='ghIcon' />GitHub</button>
+                    <button className="gitBtn" type="submit"> <FaIcons.FaGithub className='ghIcon' />GitHub</button>
                 </form>
                 <form className="contactBtns" action="https://drive.google.com/file/d/14prWGh1MwMsED8Jh8WG93YCji29t9167/view?usp=sharing" method="get" target="_blank">
-                    <button type="submit"> <FaIcons.FaRegFilePdf className='ghIcon' />Resume</button>
+                    <button className="resumeBtn" type="submit"> <FaIcons.FaRegFilePdf className='pdfIcon' />Resume</button>
                 </form>
 
             </section>
