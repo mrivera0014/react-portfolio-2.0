0c2efbb5affd929f4df00be26702d7e29d943fbc
Marble <mrivera0014@gmail.com>
2022-02-16 22:42:42 -0700

    created footer and styled

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div>
+            <footer className='footer'> &copy; 2021 Maribel Rivera </footer>
+        </div>
+    )
+}
+
+export default Footer
